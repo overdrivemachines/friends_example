25b107512f977476b8cb3c06a29f5f188bb0ff02
Dipen Chauhan <get.dipen@gmail.com>
2018-07-24 20:25:39 -0700

    Fixed not_self and not_pending validations

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -13,6 +13,52 @@ class FriendRequest < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
+  # User needs to be present
+  validates :user, presence: true
+  
+
+  # Friend needs to be present and prevent repeats of the same row
+  # f = FriendRequest.create(user_id: 2, friend_id: 3)
+  #   commit transaction
+  # f = FriendRequest.create(user_id: 2, friend_id: 3)
+  #   rollback transaction
+  # f.errors.full_messages
+  #  => ["Friend has already been taken"]
+  validates :friend, presence: true, uniqueness: { scope: :user }
+
+  validate :not_self # Don't allow friend requests to yourself
+  validate :not_friends # Don't allow friend requests if already friends
+  validate :not_pending # Don't allow friend requests if already pending
+
+  # Don't allow friend requests to yourself
+  # f = FriendRequest.create(user_id: 1, friend_id: 1)
+  # f.errors.full_messages
+  #   => ["Friend can't be equal to user"]
+  def not_self
+  	if (user_id == friend_id)
+  		errors.add(:friend, "can't be equal to user")
+  	end
+  end
+
+  def not_friends
+  	# TODO
+  end
+
+  # Don't allow friend requests if already pending
+  # Can't send a request to a friend who has already requested you to be thier friend
+  # f = FriendRequest.create(user_id: 3, friend_id: 4)
+  #   commit transaction
+  # f = FriendRequest.create(user_id: 4, friend_id: 3)
+  #   rollback transaction
+  # f.errors.full_messages
+  #   => ["Friend already requested friendship"]
+  # u4.incoming_friend_requests.include?(u3.friend_requests)
+  def not_pending
+    if user.incoming_friend_requests.pluck(:user_id).include?(friend_id)
+      errors.add(:friend, 'already requested friendship')
+    end
+  end
+
   def outgoing
 
   end
